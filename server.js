b798a1fb0578e99f44e8721a2c9a419523c48aa7
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-02-25 20:30:48 +0200

    dfdf

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/index.html'));
+// });
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
