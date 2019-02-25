d95198bad516be84ba3a64e668906ad1e6d8c950
mickaelbenaroch <mickaelbenaroch@yahoo.fr>
2019-02-25 19:50:33 +0200

    set

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8000;
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -13,4 +14,6 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log("App is running on port " + port);
+});
