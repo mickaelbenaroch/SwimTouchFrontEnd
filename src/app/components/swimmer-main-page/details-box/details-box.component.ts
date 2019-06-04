bc83bf9f31098357f31ac3d07fb91a32ffb73322
benivalotker <valotkerbeni@gmail.com>
2019-06-04 20:10:21 +0300

    fix main page swimmer style

--- src/app/components/swimmer-main-page/details-box/details-box.component.ts
+++ src/app/components/swimmer-main-page/details-box/details-box.component.ts
@@ -15,6 +15,10 @@ export class DetailsBoxComponent implements OnInit {
   public teams: TeamModel[] = [];
   public trainnings: TrainningModel[] = [];
   @Output() ShowTodayTrainningDetails: EventEmitter<TrainningModel> = new EventEmitter();
+
+  hebrew_days: string;
+   hebrew_mon: string;
+   today_days: number
   //#endregion
 
   //#region Constructor & Lyfecycle Hooks
@@ -46,6 +50,8 @@ export class DetailsBoxComponent implements OnInit {
         console.log(err);
       }
     )
+
+    this.hebrewDay();
   }
   //#endregion
 
@@ -55,6 +61,40 @@ export class DetailsBoxComponent implements OnInit {
    */
   public ShowTrainning(trainning: TrainningModel):void{
     this.ShowTodayTrainningDetails.emit(trainning);
+  }
+
+    /**
+   * hebrew day
+   */
+  public hebrewDay():void{
+    var today = new Date ( );
+    
+    var days = new Array ( );
+    days[days.length] = "יום ראשון";
+    days[days.length] = "יום שני";
+    days[days.length] = "יום שלישי";
+    days[days.length] = "יום רביעי";
+    days[days.length] = "יום חמישי";
+    days[days.length] = "יום שישי";
+    days[days.length] = "יום שבת";
+    
+    var months = new Array ( );
+    months[months.length] = "ינואר";
+    months[months.length] = "פברואר";
+    months[months.length] = "מרץ";
+    months[months.length] = "אפריל";
+    months[months.length] = "מאי";
+    months[months.length] = "יוני";
+    months[months.length] = "יולי";
+    months[months.length] = "אוגוסט";
+    months[months.length] = "ספטמבר";
+    months[months.length] = "אוקטובר";
+    months[months.length] = "נובמבר";
+    months[months.length] = "דצמבר";
+
+    this.hebrew_days = days[today.getDay()];
+    this.hebrew_mon  = months[today.getMonth()];
+    this.today_days  = today.getDate();
   }
   //#endregion
 
